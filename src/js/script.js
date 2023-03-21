fce893107245be6dc25672cf1acb6090115a8e97
AlexFili <RootyRoots1507@gmail.com>
2023-03-21 12:59:55 +0300

    the first APP(film-rate) is ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,23 @@
 'use strict';
 
-const user = 'Sasha';
-alert(`Hello, ${user}`);
+let numberOfFilms = +prompt('how many films have you seen so far?', '');
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false    
+
+};
+
+const 
+    movie_1 = prompt('name one of the last films you watched', ''),
+    rating_1 = +prompt('Give it a rating', ''),
+    movie_2 = prompt('name one of the last films you watched', ''),
+    rating_2 = +prompt('Give it a rating', '');
+
+personalMovieDB.movies[movie_1] = rating_1;
+personalMovieDB.movies[movie_2] = rating_2;
+
+
+console.log(personalMovieDB);
