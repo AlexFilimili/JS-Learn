0899ee2349de5d1f585b1d3938176cbfb9dbee03
AlexFili <RootyRoots1507@gmail.com>
2023-03-25 23:21:26 +0300

    APP whith loop is ready

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,23 +1,57 @@
 'use strict';
 
-let numberOfFilms = +prompt('how many films have you seen so far?', '');
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false    
+let numberOfFilms;
+/* не даем оставить пустую строку */
+do {
+    numberOfFilms = +prompt('how many films have you seen so far?', '');
+}
+while (numberOfFilms == '');
 
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false   
 };
 
-const 
-    movie_1 = prompt('name one of the last films you watched', ''),
-    rating_1 = +prompt('Give it a rating', ''),
-    movie_2 = prompt('name one of the last films you watched', ''),
-    rating_2 = +prompt('Give it a rating', '');
+if (personalMovieDB.count < 10) {
+    alert('very few movies have been watched');
+}
+else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    alert('your number of movies watched matches the majority');
+}
+else if (personalMovieDB.count >= 30) {
+    alert('do you really like to watch movies');
+}
 
-personalMovieDB.movies[movie_1] = rating_1;
-personalMovieDB.movies[movie_2] = rating_2;
+for(let i = 1; i <= numberOfFilms; i++) {   
+    let 
+        movie,
+        rating;
+    do {        
+    movie = prompt('name one of the last films you watched', ''),
+    rating = +prompt('Give it a rating', '');
+    }    
+    while (movie == null || movie.length === 0 || movie.length >= 50 || rating === 0 || rating.length === 0);
 
+    personalMovieDB.movies[movie] = rating; 
+    
+}
 
 console.log(personalMovieDB);
+console.log(personalMovieDB.count);
+
+
+
+
+
+
+
+
+ 
+
+
+
+
+
--- dist/js/synopsis.js
+++ dist/js/synopsis.js
@@ -85,15 +85,230 @@ console.log(5%2); /* взятие остатка */
 console.log(2+8 === 10); /* true,так как строгое равенство*/
 console.log(2+8 == '10');/* true,так как не строгое равенство*/
 
-/* оператор И */
+/* оператор И (ВОЗВРАЩАЕТ ПЕРВОЕ ЛОЖНОЕ ЗНАЧЕНИЕ, КОД ДАЛЬШЕ НЕ ИДЁТ)*/
 const 
     isChecked = true,
     isClose = true;
 console.log(isChecked && isClose); /* true, так как оба условия соблюдены */
 
-/* оператор ИЛИ */
+/* Условия, при котором ингредиенты собраны для приготовления супа */
 const 
-    isChecked = true,
-    isClose = false;
-console.log(isChecked || !isClose); /* true, так как оба условия соблюдены c помощью оператора отрицания */
+    beans = true,
+    cilantro = true,
+    tomatoes = 3,
+    zucchini = true,
+    tomatoPaste = true,
+    carrots = true,
+    sweetBellPepperRed = 2,
+    sweetBellPepperYellow = 0;
+
+    if ((sweetBellPepperRed === 2 || sweetBellPepperYellow === 2) && beans && cilantro && tomatoes === 3 && zucchini && tomatoPaste && carrots) {
+        console.log('soup is ready');
+    }
+    else {
+        console.log('We keep on shopping');
+    }
+    
+/* оператор ИЛИ (ВОЗВРАЩАЕТ ПЕРВОЕ ИСТИННОЕ ЗНАЧЕНИЕ, КОД ДАЛЬШЕ НЕ ИДЁТ) */
+const 
+    isChecked2 = true,
+    isClose2 = false;
+console.log(isChecked2 || !isClose2); /* true, так как оба условия соблюдены c помощью оператора отрицания */
+
+/* Условия */
+const num = 50;
+if (num < 50) {
+    console.log('not much');
+}
+else if (num > 50) {
+    console.log('much');
+}
+else {
+    console.log('matched');
+}
+/* Тоже самое, но с тернарным оператором */
+(num === 50) ? console.log('matched') : console.log('Error');
+
+/* А теперь через switch (строго стравнение) */
+
+switch (num) {
+    case 30:
+        console.log('not much');        
+            break;
+    case 60:
+        console.log('much');        
+            break;
+    case 50:
+        console.log('matched');        
+            break;
+    default:
+        console.log('try it next time');
+            break;
+}
+
+/* циклы */
+let num3 = 50;
+while (num3 < 55) {
+    console.log(num3);
+    num3++;
+}
+/* или так, работать будет точно также */
+let num33 = 50;
+do {
+    console.log(num33);
+    num33++;
+}
+while (num33 < 55);
+
+/* базовый и фундаментальный принцип програмирования */
+let num4 = 50;
+for (let i = 1; i < 12; i++) {
+    console.log(num4);
+    num4++;
+}
+/* прерываем цикл */
+let num5 = 50;
+for (let i = 1; i < 12; i++) {
+    console.log(num5);
+    num5++;
+    if (num5 === 52) break;
+}
+
+/* пропустим часть цикла */
+
+for (let i = 1; i < 12; i++) {    
+    if (i >= 3 && i<=9 ) {
+        continue;
+    }
+    console.log(i);
+}
+
+/* вложенный цикл, рисуем пирамидку */
+let result = '';
+const length = 7;
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += '*';
+    }
+    result += '\n';   
+}
+console.log(result);
+
+/* выводим четные числа */
+for (let i = 2; i <= 10; i++) {    
+    if (i % 2 === 1) {
+        continue;
+    }
+    console.log(i);
+}
+
+/* выводим нечетные через WHILE */
+let i = 2;
+while (i < 16) {    
+    i++;
+    if (i % 2 === 0) {
+        continue;
+    }
+    else {
+        console.log(i);
+    }
+}
+
+/* записываем в массив цифры от 5 до 10 */
+const arrayOfNumbers = [];
+let numb = 5;
+let j11 = 0;
+let i11 = 0;
+
+do {
+    arrayOfNumbers[j11] = numb;
+    i11++;
+    j11++;
+    numb++;
+}
+while (i11 <= 5);
+  
+console.log(arrayOfNumbers);
+
+/* а теперь через For */
+const arrayOfNumbers22 = [];
+let num22 = 5;
+let j22 = 0;
+for (let i = 0; i <=5; i++) {
+    arrayOfNumbers[j22] = num22;
+    num22++;
+    j22++;
+}
+console.log(arrayOfNumbers22);
+
+/* перекладываем из одного массива в другой */
+function massive() {
+
+    const arr = [3, 5, 8, 16, 20, 23, 50];
+    const result = [];
+    let j = 0; 
+    let i = 0;
+
+    do {
+        result[j] = arr[i];
+        i++;
+        j++;
+        if (j >= arr.length) {
+            break;
+        }
+    }
+    while (i <= arr.length);
+    console.log(result);
+
+}
+ massive();
+
+ /* изменяем массив с разными данными */
+function massive2() {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    let length = data.length;
+
+    for (let i = 0; i < length; i++) {
+        let a = data[i];
+        let aMod;   
+        if ( typeof a == 'number') {
+            aMod = a * 2;
+        } 
+        else if ( typeof a == 'string') {
+            aMod = `${a} - done`;
+        } 
+        data[i] = aMod;
+    }
+    console.log(data);
+}
+massive2();
+
+/* разворачиваем массив */ 
+
+function massive3 {
+    const data = [5, 10, 'Shopping', 20, 'Homework'];
+    const result = [];
+    let length = data.length;
+    let j = 4;
+
+    for (let i = 0; i < length; i++) {
+        let dataD = data[i];
+        result[j] = dataD;
+        j--; 
+    }
+    console.log(result);
+}
+massive3();
+
+
+
+
+
+
+
+
+
+
+
+
 
